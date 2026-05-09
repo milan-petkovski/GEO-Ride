0077eeadf46fe9d83730bf5593638ba1c16e28ad
Milan Petkovski <milan.petkovski1969@gmail.com>
2026-05-09 16:47:51 +0200

    Fix: Add esbuild dependency and simplify build config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    minify: 'esbuild',
+    minify: true,
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
