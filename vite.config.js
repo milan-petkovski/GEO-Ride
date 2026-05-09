4ac63a63cb7bf871092730c147d1770f4357c8b0
Milan Petkovski <milan.petkovski1969@gmail.com>
2026-05-10 00:50:22 +0200

    Improve accessibility and resolve static asset 404 errors

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  base: './',
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    minify: true,
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: './',
+    server: {
+      port: 3000,
+      open: true
+    },
+    build: {
+      outDir: 'dist',
+      minify: true,
+      chunkSizeWarningLimit: 1600,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
           }
         }
       }
+    },
+    define: {
+       'import.meta.env.VITE_MAPBOX_TOKEN': JSON.stringify(process.env.MAPBOX_TOKEN || process.env.VITE_MAPBOX_TOKEN)
     }
-  },
-  define: {
-    'import.meta.env.VITE_MAPBOX_TOKEN': JSON.stringify(process.env.MAPBOX_TOKEN || process.env.VITE_MAPBOX_TOKEN)
-  }
+  };
 });
