10110670db32beb2b595f1d40d45a324be7be3fd
Milan Petkovski <milan.petkovski1969@gmail.com>
2026-05-14 00:37:31 +0200

    Use default minifier for Vite 8

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'dist',
-      minify: 'terser',
+      minify: true,
       cssMinify: true,
       reportCompressedSize: false,
       chunkSizeWarningLimit: 2000,
